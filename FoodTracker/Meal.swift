349aff2289f1168aa8e59efef764a3fe7bb65bcd
Jacob Sephton <jacob.sephton@gmail.com>
2020-09-21 20:49:10 +1000

    SUMMARY: Prepare for persistance implementation; minor UI tweak
    DETAILS:
    Scale to fill image view in table view cell, for visual consistency
    Make Meal a subclass of NSObject and NSCoding in preparation for data persistance
    Add archiving paths to Meal in prep. for persistance
    
    ISSUES:
    nil

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
     
@@ -16,9 +17,25 @@ class Meal {
     var photo: UIImage?
     var rating: Int
     
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
+    //MARK: Archiving Paths
+    
+    // urls(for:in:) returns an array of URLS in the document directory, accessible to the user. The first option of this array is an optional containing the first URL. However, the returned array should always contain exactly one match, so we can force-unwrap by appending `!`
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
     //MARK: Initialization
     
     init? (name: String, photo: UIImage?, rating: Int) {
+        
+        
         self.name = name
         self.photo = photo
         self.rating = rating
@@ -33,6 +50,31 @@ class Meal {
             return nil
         }
         
+        super.init()
         
+        
+    }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // the name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object", log: OSLog.default, type: .debug)
+            return nil
+        }
+        // Because photo is an optional property of Meal, just use conditional cast
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        // Must call designated initializer
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
